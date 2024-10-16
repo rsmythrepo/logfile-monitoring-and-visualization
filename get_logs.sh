758b54cee9a270662250ff8ed6827751405441b1
rsmythrepo <raphaelle.smyth@gmail.com>
2024-10-16 19:17:46 +0000

    get data

--- get_logs.sh
+++ get_logs.sh
@@ -1,7 +1,14 @@
 logfile="logfile.txt"
-logfiletest="logtest.txt"
+logfiletest="logtest3.txt"
 kubectl logs deploy/orderbookapi -n c402-team01-dev --since=2h > "$logfile"
 
+# Define output CSV files
+messages_output="messages_output.csv"
+http_logs_output="http_logs_output.csv"
+
+# Initialize output files with headers
+echo "fix_version,MsgType,MsgSeqNum,SenderCompID,TargetCompID,SendingTime,HeartBtInt,CheckSum" > "$messages_output"
+echo "IP_Address,Port,HTTP_Method,Path,Status_Code,Time_Value,Stock" > "$http_logs_output"
 
 # Variables for parsed data
 fix_version=""
@@ -39,47 +46,9 @@ do
         	SendingTime=$(echo "$line" | grep -o "52=[^;]*" | cut -d= -f2 | sed 's/T/ /')
         	HeartBtInt=$(echo "$line" | grep -o "108=[^;]*" | cut -d= -f2)
         	CheckSum=$(echo "$line" | grep -o "10=[^;]*" | cut -d= -f2)
-        	Stock_Symbol=$(echo "$line" | grep -o "55=[^;]*" | cut -d= -f2)
-        	OrderQty=$(echo "$line" | grep -o "38=[^;]*" | cut -d= -f2)
-        	TransactTime=$(echo "$line" | grep -o "60=[^;]*" | cut -d= -f2 | sed 's/T/ /')
-        	Side=$(echo "$line" | grep -o "54=[^;]*" | cut -d= -f2)
-        	Price=$(echo "$line" | grep -o "44=[^;]*" | cut -d= -f2)
-        	SenderSubID=$(echo "$line" | grep -o "50=[^;]*" | cut -d= -f2)
 
-	# Optional fields with fallback values
-	Stock_Symbol=$(echo "$line" | grep -oP '(?<=/stock_quote\?symbol=)[^ ]+')
-        #Stock_Symbol=$(echo "$line" | grep -o "55=[^;]*" | cut -d= -f2)
-        OrderQty=$(echo "$line" | grep -o "38=[^;]*" | cut -d= -f2)
-        TransactTime=$(echo "$line" | grep -o "60=[^;]*" | cut -d= -f2 | sed 's/T/ /')
-        Side=$(echo "$line" | grep -o "54=[^;]*" | cut -d= -f2)
-        Price=$(echo "$line" | grep -o "44=[^;]*" | cut -d= -f2)
-        SenderSubID=$(echo "$line" | grep -o "50=[^;]*" | cut -d= -f2)
-
-        # Use fallback if fields are not found
-        #Stock_Symbol=${Stock_Symbol:-""}
-        OrderQty=${OrderQty:-""}
-        TransactTime=${TransactTime:-""}
-        Side=${Side:-""}
-        Price=${Price:-""}
-        SenderSubID=${SenderSubID:-""}
-	# Print extracted values
-	
-	echo "--------------------------------"
-            echo "FIX Version: $fix_version"
-            echo "MsgType: $MsgType"
-            echo "MsgSeqNum: $MsgSeqNum"
-            echo "SenderCompID: $SenderCompID"
-            echo "TargetCompID: $TargetCompID"
-            echo "SendingTime: $SendingTime"
-            echo "HeartBtInt: $HeartBtInt"
-            echo "CheckSum: $CheckSum"
-            echo "Stock Symbol: $Stock_Symbol"
-            echo "OrderQty: $OrderQty"
-            echo "TransactTime: $TransactTime"
-            echo "Side: $Side"
-            echo "Price: $Price"
-            echo "SenderSubID: $SenderSubID"
-            echo "----------------------------------"
+		# Write FIX Message data to messages_output.csv
+        	echo "$fix_version,$MsgType,$MsgSeqNum,$SenderCompID,$TargetCompID,$SendingTime,$HeartBtInt,$CheckSum" >> "$messages_output"
 	fi
 
     	# Parse HTTP Logs
@@ -90,27 +59,19 @@ do
         	Path=$(echo "$line" | grep -oP '[^ ]+ HTTP' | sed 's/ HTTP//')
         	Status_Code=$(echo "$line" | grep -oP '(?<=HTTP/1.1" )\d{3}')
 		endpoint=$(echo "$line" | grep -oP '(?<=GET /)[^?]+')
-		# Set flag to indicate that the next line contains the time value
-        	
+		Stock=$(echo "$line" | grep -oP '(?<=symbol=)[A-Z]+')
 
+
+	# Set flag to indicate that the next line contains the time value
 	elif [[ $line =~ \"time\": ]] && [[ $line =~ \"path\": ]]; then
-
 		json_path=$(echo "$line" | grep -oP '(?<=path": "/)[^"]+')
 
 		if [[ "$json_path" == "$endpoint" ]]; then
 			Time_Value=$(echo "$line" | grep -oP '(?<="time": )\d+\.\d+')
 		fi
-		# Print HTTP Log Values
-        	echo "----------------------------------"
-        	echo "IP Address: $IP_Address"
-        	echo "Port: $Port"
-        	echo "HTTP Method: $HTTP_Method"
-        	echo "Path: $Path"
-        	echo "Status Code: $Status_Code"
-        	echo "Time_Value: $Time_Value"
-		echo "Endpoint: $endpoint"
-		echo "Path: $json_path"
-        	echo "----------------------------------"
+
+		# Write HTTP Log data to http_logs_output.csv
+        	echo "$IP_Address,$Port,$HTTP_Method,$Path,$Status_Code,$Time_Value,$Stock" >> "$http_logs_output"
 	fi 
 
 
